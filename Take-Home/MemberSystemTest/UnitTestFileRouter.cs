db7349e061c6b9970c029f7b862f45326baecac3
寬寬 <polu635241@gmail.com>
2024-05-14 07:59:06 +0800

    新增單元測試專案 新增FileRouter測試

---
+++ Take-Home/MemberSystemTest/UnitTestFileRouter.cs
@@ -0,0 +1,89 @@
+using MemberSystemSimulator.Tool;
+
+namespace MemberSystemTest
+{
+    [Collection ("UnitTestFileRouter")]
+    public class UnitTestFileRouter
+    {
+        [System.Serializable]
+        class UnitTestData 
+        {
+            public string name;
+
+            public UnitTestData (string name)
+            {
+                this.name = name;
+            }
+        }
+
+        [Fact]
+        public void TestAdd ()
+        {
+            try 
+            {
+                FileRouter<UnitTestData>.DeleteAll ();
+
+                var newOne = new UnitTestData ("testAdd");
+
+                FileRouter<UnitTestData>.Add (newOne);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail (e.Message + e.StackTrace);
+            }
+
+            //如何長度為0就是新增失敗
+            Assert.False (FileRouter<UnitTestData>.caches.Count == 0, $"新增失敗");
+        }
+
+        [Fact]
+        public void TestDelete ()
+        {
+            int oldCount = 0;
+            int newCount = 0;
+
+            try
+            {
+                var newOne = new UnitTestData ("testDelete");
+
+                FileRouter<UnitTestData>.Add (newOne);
+
+                //比對刪除前後數字確定真的刪除了
+                oldCount = FileRouter<UnitTestData>.caches.Count;
+
+                FileRouter<UnitTestData>.Delete (newOne);
+
+                newCount = FileRouter<UnitTestData>.caches.Count;
+            }
+            catch (Exception e)
+            {
+                Assert.Fail (e.Message + e.StackTrace);
+            }
+
+            //刪除完要剛好少1
+            Assert.False (newCount != oldCount - 1, "刪除測試失敗");
+        }
+
+        [Fact]
+        public void TestClear()
+        {
+            try
+            {
+                var newOne = new UnitTestData ("testClear");
+
+                FileRouter<UnitTestData>.Add (newOne);
+
+                Assert.False (FileRouter<UnitTestData>.caches.Count == 0, $"新增失敗 {FileRouter<UnitTestData>.caches.Count}");
+
+                FileRouter<UnitTestData>.DeleteAll ();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail (e.Message + e.StackTrace);
+            }
+
+            //如果長度為0就是新增失敗
+            Assert.False (FileRouter<UnitTestData>.caches.Count != 0, $"刪除失敗 {FileRouter<UnitTestData>.caches.Count}");
+        }
+    }
+}
